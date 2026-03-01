e58eb009df3e2f911b11a8f0b585ffb7a8e54920
kimsohea <kimsohea4425@gmail.com>
2026-03-01 10:13:09 +0900

    fix: 포트폴리오 파라미터 처리

--- src/components/License.vue
+++ src/components/License.vue
@@ -13,24 +13,26 @@
 </template>
 
 <script setup>
-  const licenseArr = [
-    {
-      time: '',
-      title: '포트폴리오 페이지',
-      type: '사이트',
-      link: 'https://kimsohea.github.io/code-experience/',
-    },
-    {
-      time: '2019.11',
-      title: '웹디자인기능사 취득',
-      type: '자격증',
-      link: '',
-    },
-    {
-      time: '2019.08',
-      title: 'GTQ(그래픽기술자격)1급 취득',
-      type: '자격증',
-      link: '',
-    }
-  ]
+const type = new URLSearchParams(window.location.search).get("type") || "";
+const career = type === "FE" ? "?type=FE" : "";
+const licenseArr = [
+  {
+    time: "",
+    title: "포트폴리오 페이지",
+    type: "사이트",
+    link: `https://kimsohea.github.io/code-experience/${career}`,
+  },
+  {
+    time: "2019.11",
+    title: "웹디자인기능사 취득",
+    type: "자격증",
+    link: "",
+  },
+  {
+    time: "2019.08",
+    title: "GTQ(그래픽기술자격)1급 취득",
+    type: "자격증",
+    link: "",
+  },
+];
 </script>
